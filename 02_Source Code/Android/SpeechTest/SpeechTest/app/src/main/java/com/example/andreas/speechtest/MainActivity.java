4d59956e015b1bccd69e218751129255b72eacf6
ErgoTobi <bartsch.tobias@gmx.de>
2017-11-21 17:33:28 +0100

    Speech controlled Braccio V1.0 draft

---
+++ 02_Source Code/Android/SpeechTest/SpeechTest/app/src/main/java/com/example/andreas/speechtest/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.andreas.speechtest;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.speech.RecognizerIntent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+import java.util.Locale;
+
+import static java.lang.Thread.sleep;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int SPEECH_REQUEST_CODE = 0;
+    private TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textfield);
+        Button button = (Button) findViewById(R.id.button);
+
+
+
+
+            /* try {
+                this.wait(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }*/
+        }
+
+
+
+    public void onClick(View v){
+
+        displaySpeechRecognizer();
+    }
+
+    // Create an intent that can start the Speech Recognizer activity
+    private void displaySpeechRecognizer() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "");
+// Start the activity, the intent will be populated with the speech text
+        try {
+            startActivityForResult(intent, SPEECH_REQUEST_CODE);
+        }
+        catch(ActivityNotFoundException e){
+            String appPackageName = "com.google.android.googlequicksearchbox";
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+            }
+            Toast.makeText(MainActivity.this, "Fehler", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    // This callback is invoked when the Speech Recognizer returns.
+// This is where you process the intent and extract the speech text from the intent.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK) {
+            List<String> results = data.getStringArrayListExtra(
+                    RecognizerIntent.EXTRA_RESULTS);
+            String spokenText = results.get(0);
+            text.setText(spokenText);
+        }
+
+    }
+}
