f88dcaf908a027069e3a49e499732d42315c1d2b
Krist <kstoja@github.com>
2017-12-06 12:35:43 +0100

    Barista_20

---
+++ 02_Source Code/Barista_20/app/src/main/java/com/example/krist/barista_20/HelpActivity.java
@@ -0,0 +1,44 @@
+package com.example.krist.barista_20;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Krist on 06/12/2017.
+ */
+
+public class HelpActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+}
