37e4d830fb1ba2695ba7fd0828fbba994f3f269a
KStoja <33523261+KStoja@users.noreply.github.com>
2017-12-15 13:06:07 +0100

    popup logik

--- 02_Source Code/Barista_30/app/src/main/java/com/example/andreas/barista_30/MainActivity.java
+++ 02_Source Code/Barista_30/app/src/main/java/com/example/andreas/barista_30/MainActivity.java
@@ -29,7 +29,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
 
     private SectionsPageAdapter mSectionsPageAdapter;
